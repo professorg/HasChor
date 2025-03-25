cbaf9ae4ad198a1456c6ccb4260d406ff5c04f8f
Grant VanDomelen <grant.vandomelen@gmail.com>
2025-03-25 15:17:54 -0700

    Static analysis counting send/recv events

--- examples/optimization/Main.hs
+++ examples/optimization/Main.hs
@@ -214,6 +214,15 @@ choreo_opt_epp_IO = choreo_opt_epp
 choreo_opt_run_IO :: Kleisli IO () (Integer @ "alice", Integer @ "alice")
 choreo_opt_run_IO = runChoreo choreo_opt
 
+count_send_recv :: Network ar a b -> (Integer, Integer)
+count_send_recv (Comp f e k) =
+  let (s, r) = count_send_recv k in
+    case e of
+      Send _ -> (s + 1, r)
+      Recv _ -> (s, r + 1)
+      _ -> (s, r)
+count_send_recv (Hom _) = (0, 0)
+
 main' :: Choreo (Kleisli IO) () (Integer @ "alice", Integer @ "alice") -> IO ()
 main' c = do
   config <- mkLocalConfig locs
